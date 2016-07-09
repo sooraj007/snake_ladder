bfcc093388d5148313c22a7512911a77fc26fa32
Sooraj Narayan <proflicxx@gmail.com>
2016-07-10 01:35:59 +0530

    folder structure rearanged changed file names

---
+++ js/engine.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	var i = 36;
+	
+	$('.boardTable td').each(function(index)
+	{
+		$(this).append("<span style='padding-left: 50%;;color:white'>"+i--+"</span")
+		
+ });
+	
+	
+});
